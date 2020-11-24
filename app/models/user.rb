b3fb9daf83506d3e2bd818264637a0b25c133977
下田　佳紀 <y.s@platinum.local>
2020-11-24 13:25:15 +0900

    has_secure_password追加/passwordバリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
     length: { maximum: 50 },
     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
     uniqueness: true
-  # has_secure_password
-  # validates :password,
-  #   presence: true,
-  #   length: { minimum: 8, maximum: 16 },
-  #   format: { with: /\A[a-z\d]{8,16}+\z/i }
+  has_secure_password
+  validates :password,
+    presence: true,
+    length: { minimum: 8, maximum: 16 },
+    format: { with: /\A[a-z\d]{8,16}+\z/i }
   # has_many :pictures
 end
