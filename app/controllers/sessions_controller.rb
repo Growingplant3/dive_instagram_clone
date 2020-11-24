a8fdc105b4717275f46551ff81bfc3e8b96d9617
下田　佳紀 <y.s@platinum.local>
2020-11-24 14:53:58 +0900

    createアクション記述

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    binding.pry
+    if user && user.authenticate(params[:session][:email])
+      session[:user_id] = user.id
+      flash[:notice] = "ログインに成功しました。"
+      redirect_to user_path(user.id)
+    else
+      flash[:danger] = "ログインに失敗しました。"
+      render :new
+    end
+  end
 end
