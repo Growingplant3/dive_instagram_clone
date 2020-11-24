c91020104dd45c872e669fdac45eee311f4ba7cc
下田　佳紀 <y.s@platinum.local>
2020-11-24 11:02:38 +0900

    モデル作成、軽めのバリデーション設定

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  before_validation { email.downcase! }
+  validates :name,
+    presence: true,
+    length: { maximum: 20 }
+  validates :email,
+    presence: true,
+    length: { maximum: 50 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+    uniqueness: true
+  # has_secure_password
+  # validates :password,
+  #   presence: true,
+  #   length: { minimum: 8, maximum: 16 },
+  #   format: { with: /\A[a-z\d]{8,16}+\z/i }
+  # has_many :pictures
+end
