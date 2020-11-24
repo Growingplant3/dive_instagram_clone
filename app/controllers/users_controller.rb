f5e3abd6bbf9bc2de7fed03772344cd7cd1804a8
下田　佳紀 <y.s@platinum.local>
2020-11-24 14:03:49 +0900

    flashメッセージ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] = "アカウントの作成が成功しました。"
       redirect_to users_path
     else
+      flash[:notice] = "アカウントの作成が失敗しました。"
       render :new
     end
   end
