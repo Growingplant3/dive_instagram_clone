cdd300f1ae691ed7ff1e3b9ee4f89e0d3bdb732d
下田　佳紀 <y.s@platinum.local>
2020-11-24 18:28:43 +0900

    create後のshowへ遷移整備

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    binding.pry
-    if user && user.authenticate(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:notice] = "ログインに成功しました。"
       redirect_to user_path(user.id)
