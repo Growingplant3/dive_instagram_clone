3a4e0e103cd5a0801e92e1be621d8ee5fe77df6c
下田　佳紀 <y.s@platinum.local>
2020-11-24 13:44:51 +0900

    モデルのnameカラムのバリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   before_validation { email.downcase! }
-  validates :name,
-    presence: true,
-    length: { maximum: 20 }
+  validates :name, length: { maximum: 20 }
   validates :email,
     presence: true,
     length: { maximum: 50 },
