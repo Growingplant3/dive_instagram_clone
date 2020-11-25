632ec5445ec69cabe6bd2bb8e93f2eec1c248453
下田　佳紀 <y.s@platinum.local>
2020-11-25 23:06:34 +0900

    createアクション整備/newアクションの不備修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,21 @@
 class PicturesController < ApplicationController
   def new
-    @picture = Picture.new
+    @picture = current_user.pictures.build
+  end
+
+  def create
+    @picture = current_user.pictures.build(picture_params)
+    if @picture.save(picture_params)
+      flash[:notice] = "コメント/画像の登録が成功しました。"
+      redirect_to pictures_path
+    else
+      flash.now[:danger] = "コメント/画像の登録が失敗しました。"
+      render :new
+    end
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:content, :image, :user_id)
   end
 end
