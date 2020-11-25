bba490a84b86055087633558a6f3ee67e9e303ab
下田　佳紀 <y.s@platinum.local>
2020-11-25 14:23:19 +0900

    updateアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit]
+  before_action :set_user, only: %i[show edit update]
   def new
     @user = User.new
   end
@@ -19,6 +19,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    @user = User.new(user_params)
+    if @user.update
+      flash[:notice] = "アカウントの編集が成功しました。"
+      redirect_to user_path(@user)
+    else
+      flash.now[:danger]
+      render :edit
+    end
   end
 
   private
