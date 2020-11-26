049092b27854c46106e78e0ea0e7be949df658bb
下田　佳紀 <y.s@platinum.local>
2020-11-27 00:02:54 +0900

    ルート整備

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :pictures do
     post :confirm, on: :collection
   end
-  resources :favorites, only: %i[create destroy]
+  resources :favorites, only: %i[create destroy index]
   root to: "users#new"
 end
