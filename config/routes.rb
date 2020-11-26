8b70fb7f73b880c78a4782f4e014653c4b6d6236
下田　佳紀 <y.s@platinum.local>
2020-11-26 11:25:44 +0900

    confirmのルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users, except: [:index]
   resources :sessions, only: %i[new create destroy]
-  resources :pictures
+  resources :pictures do
+    post :confirm, on: :collection
+  end
   root to: "users#new"
 end
