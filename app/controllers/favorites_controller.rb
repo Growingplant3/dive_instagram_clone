04a1bbc92bf9e6b748dc900207740f28fdb84b63
下田　佳紀 <y.s@platinum.local>
2020-11-26 23:56:04 +0900

    create/destroyアクションを整備

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,13 @@
 class FavoritesController < ApplicationController
+  def create
+    favorite = current_user.favorites.create(picture_id: params[:picture_id])
+    flash[:notice] = "#{favorite.picture.user.email}さんの投稿をお気に入り登録しました。"
+    redirect_to pictures_path
+  end
+
+  def destroy
+    favorite = current_user.favorites.find(params[:id]).destroy
+    flash[:danger] = "#{favorite.picture.user.email}さんの投稿をお気に入り解除しました。"
+    redirect_to pictures_path
+  end
 end
