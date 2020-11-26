0560dddc445c2d424cd1c5c9a9faafc1c8958063
下田　佳紀 <y.s@platinum.local>
2020-11-26 17:38:23 +0900

    誤字修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :pictures do
     post :confirm, on: :collection
   end
-  recources :favorites, only: %i[create destroy]
+  resources :favorites, only: %i[create destroy]
   root to: "users#new"
 end
