54aed62325a2ab702817bb6c380a17b2fb04136f
下田　佳紀 <y.s@platinum.local>
2020-11-28 00:48:48 +0900

    jquery-ujs読み込み

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require rails-ujs
 //= require turbolinks
 //= require jquery/dist/jquery.js
+//= require jquery-ujs
 //= require popper.js/dist/popper.js
 //= require bootstrap/dist/js/bootstrap.js
 //= require_tree .
