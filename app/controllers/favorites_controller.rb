53387e7efb60d3c07b3f69730878498c98e64345
下田　佳紀 <y.s@platinum.local>
2020-11-28 20:43:43 +0900

    デバッグを削除

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -12,7 +12,6 @@ class FavoritesController < ApplicationController
   end
 
   def index
-    flash[:notice] = "通過"
     favorites_picture_ids = current_user.favorites.pluck(:picture_id)
     @pictures = Picture.where(id: favorites_picture_ids)
   end
